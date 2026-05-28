5bd23fe03f999196f37cc43096b6cf6511362cb7
Sabya <sdash0113@gmail.com>
2026-05-28 09:04:26 +0530

    Automated the facebook login using selenium

--- navigation.py
+++ navigation.py
@@ -61,58 +61,85 @@
 # driver.quit()
 
 
-# css and selenium selectors uses
+# #css and selenium selectors uses
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import time
+#
+# driver=webdriver.Chrome()
+# driver.get('https://rahulshettyacademy.com/AutomationPractice/')
+#
+# driver.maximize_window()
+# name_box=driver.find_element(By.ID,'autocomplete'
+#                              )
+# name_box.send_keys('Australia')
+# time.sleep(2)
+#
+# name_box.clear()
+# time.sleep(1)
+#
+# name_box.send_keys('India')
+# driver.find_element(By.XPATH,"//input[@value='radio2']").click()
+# time.sleep(1)
+#
+# checkbox=driver.find_element(By.XPATH,'/html/body/div[1]/div[4]/fieldset/label[3]/input')
+# checkbox.click()
+# time.sleep(1)
+#
+# heading=driver.find_element(By.TAG_NAME,'h1').text
+# print(heading)
+#
+# place_name=name_box.get_attribute('class')
+# print(place_name)
+#
+# textbox=driver.find_element(By.ID,'displayed-text')
+# print('Before hide:',textbox.is_displayed())
+#
+# driver.find_element(By.ID,'hide-textbox').click()
+# time.sleep(1)
+#
+# print('After hide:',textbox.is_displayed())
+# driver.find_element(By.ID,'show-textbox').click()
+# time.sleep(1)
+#
+# print('Textbox enable:',textbox.is_enabled())
+# print('Checkbox selected:',checkbox.is_selected())
+# time.sleep(2)
+# driver.quit()
+
+
+
+# # Assignment ( Full automation for Facebook login )
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver=webdriver.Chrome()
-driver.get('https://rahulshettyacademy.com/AutomationPractice/')
-
+# Step 1: Open Chrome
+driver = webdriver.Chrome()
 driver.maximize_window()
-name_box=driver.find_element(By.ID,'autocomplete'
-                             )
-name_box.send_keys('Australia')
-time.sleep(2)
 
-name_box.clear()
+# Step 2: Open Facebook login page
+driver.get("https://www.facebook.com/login/")
+time.sleep(3)
+
+# Step 3: Enter email
+email = driver.find_element(By.NAME, "email")
+email.clear()
+email.send_keys("sabyasachidash2017@gmail.com")   # replace with your email
 time.sleep(1)
 
-name_box.send_keys('India')
-driver.find_element(By.XPATH,"//input[@value='radio2']").click()
+# Step 4: Enter password
+password = driver.find_element(By.NAME, "pass")
+password.clear()
+password.send_keys("01a4853")       # replace with your password
 time.sleep(1)
 
-checkbox=driver.find_element(By.XPATH,'/html/body/div[1]/div[4]/fieldset/label[3]/input')
-checkbox.click()
-time.sleep(1)
+# Step 5: Click Login button
+driver.find_element(By.XPATH, "//span[text()='Log in']")
 
-heading=driver.find_element(By.TAG_NAME,'h1').text
-print(heading)
-
-place_name=name_box.get_attribute('class')
-print(place_name)
-
-textbox=driver.find_element(By.ID,'displayed-text')
-print('Before hide:',textbox.is_displayed())
-
-driver.find_element(By.ID,'hide-textbox').click()
-time.sleep(1)
-
-print('After hide:',textbox.is_displayed())
-driver.find_element(By.ID,'show-textbox').click()
-time.sleep(1)
-
-print('Textbox enable:',textbox.is_enabled())
-print('Checkbox selected:',checkbox.is_selected())
-time.sleep(2)
+driver.find_element(By.XPATH, "//span[text()='Log in']").click()
+time.sleep(10)
 driver.quit()
-
-
-
-
-
-
-
 
 
 
